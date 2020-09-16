8658118365ac1b59cd03a9bb7695c8acf89416df
Arnav Bagchi <2018.arnav.bagchi@ves.ac.in>
2020-09-16 19:33:59 +0530

    1491. Average Salary Excluding the Minimum and Maximum Salary
    Given an array of unique integers salary where salary[i] is the salary of the employee i.
    
    Return the average salary of employees excluding the minimum and maximum salary.

---
+++ Arrays/1491. Average Salary Excluding the Minimum and Maximum Salary.py
@@ -0,0 +1,6 @@
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        salary.remove(max(salary))
+        salary.remove(min(salary))
+        return sum(salary)/len(salary)
+        
