755503361b63123b26bb3dbfbdeab7bee9b4e42d
Arnav Bagchi <2018.arnav.bagchi@ves.ac.in>
2020-10-11 18:02:29 +0530

    198. House Robber

---
+++ Dynamic Programming/198. House Robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        if n>2:
+            dp=[0]*(n)
+            dp[0]=nums[0]
+            dp[1]=max(nums[0],nums[1])
+            for i in range(2,n):
+                dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+            return dp[n-1]
+        elif n==2:
+            return max(nums[0],nums[1])
+        elif n==1:
+            return nums[0]
+        else:
+            return 0
+                         
