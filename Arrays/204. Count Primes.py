a38d9370eaa8e2bfdf5116a0e1e17531d80d37cf
Arnav Bagchi <2018.arnav.bagchi@ves.ac.in>
2020-10-13 09:04:23 +0530

    204. Count Primes

---
+++ Arrays/204. Count Primes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        a=[]
+        def isPrime(n):  
+            if (n <= 1) : 
+                return False
+            if (n <= 3) : 
+                return True
+            if (n % 2 == 0 or n % 3 == 0) : 
+                return False
+            i = 5
+            while(i * i <= n) : 
+                if (n % i == 0 or n % (i + 2) == 0) : 
+                    return False
+                i = i + 6
+            return True
+        for i in range(1,n):
+            if isPrime(i)==True:
+                a.append(1)
+        return sum(a)
+        
