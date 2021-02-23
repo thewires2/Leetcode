9acd84cd80128383a7fb0a2cb4d03bc901decbc2
Arnav Bagchi <2018.arnav.bagchi@ves.ac.in>
2021-02-23 08:35:10 +0530

    Create 342. Power of Four.py

---
+++ Recursion/342. Power of Four.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n<=0:
+            return False
+        while n%4==0:
+            n=n//4
+        if n==1:
+            return True
