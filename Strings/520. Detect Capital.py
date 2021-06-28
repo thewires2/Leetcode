6af7ec7ed045120e2c5411c728511c8cce3ad079
Arnav Bagchi <2018.arnav.bagchi@ves.ac.in>
2021-06-28 12:18:18 +0530

    Create 520. Detect Capital.py

---
+++ Strings/520. Detect Capital.py
@@ -0,0 +1,8 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word==word.upper():
+            return True
+        elif word==word.lower():
+            return True
+        elif word==word.capitalize():
+            return True
