522c873c71610dc593c408b451c12e6a5d43f841
Arnav Bagchi <2018.arnav.bagchi@ves.ac.in>
2021-07-06 19:22:17 +0530

    Create 142. Linked List Cycle II.py

---
+++ Linked List/142. Linked List Cycle II.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        
+        x={}
+        
+        while head:
+            if head not in x:
+                x[head]=head.val
+                head=head.next
+            else:
+                return head
+        return None
