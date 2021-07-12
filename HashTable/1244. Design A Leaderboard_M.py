01b415241917ed7e22b774aa1f3efa413e767f29
Arnav Bagchi <2018.arnav.bagchi@ves.ac.in>
2021-07-12 09:24:21 +0530

    Create 1244. Design A Leaderboard_M.py

---
+++ HashTable/1244. Design A Leaderboard_M.py
@@ -0,0 +1,22 @@
+class Leaderboard:
+
+    def __init__(self):
+        self.scores = defaultdict()
+
+    def addScore(self, playerId: int, score: int) -> None:
+        if playerId not in self.scores:
+            self.scores[playerId] = 0
+        self.scores[playerId] += score
+
+    def top(self, K: int) -> int:
+        values = [v for _, v in sorted(self.scores.items(), key=lambda item: item[1])]
+        values.sort(reverse=True)
+        total, i = 0, 0
+        while i < K:
+            total += values[i]
+            i += 1
+        
+        return total
+
+    def reset(self, playerId: int) -> None:
+        self.scores[playerId] = 0
